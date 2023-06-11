2b598f54b650c2cbef1992a3c43663a61791a8bc
kajal <51613822+Kajuglotra@users.noreply.github.com>
2023-06-11 10:12:05 +0530

    deleted

--- FoodApplication/Food-Delivery/merchant-service/src/main/java/org/gfg/MerchantService/MerchantRepository.java
+++
@@ -1,7 +0,0 @@
-package org.gfg.MerchantService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MerchantRepository extends JpaRepository<Restaurant, Integer> {
-
-}
--- FoodApplication/Food-Delivery/utils/src/main/java/org/gfg/utils/CommonConstants.java
+++
@@ -1,7 +0,0 @@
-package org.gfg.utils;
-
-public class CommonConstants {
-    public static final String USER_CREATION_TOPIC ="user_created";
-    public static final String USER_CREATION_NAME ="name";
-    public static final String USER_CREATION_EMAIL ="email";
-}
